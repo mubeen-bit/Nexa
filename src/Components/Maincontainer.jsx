52ae950d0762031adb591a9cb2a0967e294e22e1
mubeen-bit <mubeen95155@gmail.com>
2026-06-04 00:59:13 +0100

    Heading updated

--- src/Components/Maincontainer.jsx
+++ src/Components/Maincontainer.jsx
@@ -7,21 +7,20 @@ const Maincontainer = ({ search, setSearch }) => {
     <container id="main-container">
       <div className="heading">
         <h1>
-          Get Ahead with the right information and guidance from your senior.
+          No more confusion about "How is this college?" — hear it straight from
+          your seniors and learn how to make the most of it.
         </h1>
       </div>
 
       <div className="para">
-        <p>
-          Choose from the verified seniors below and take the right next step.
-        </p>
+        <p>Choose from the verified seniors and take the right next step.</p>
       </div>
 
       <div className="s-btn">
         <input
           type="text"
           name="college"
-          placeholder="Search by college or Senior name"
+          placeholder="Search by college or Senior"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
