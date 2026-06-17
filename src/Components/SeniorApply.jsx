cece8f422d5b1e3e9e4dc09dc123b98096332e50
mubeen-bit <mubeen95155@gmail.com>
2026-06-17 16:18:18 +0100

    Senior Login and logout updated

--- src/Components/SeniorApply.jsx
+++ src/Components/SeniorApply.jsx
@@ -227,7 +227,7 @@ export default function SeniorApply() {
             <div className="popup-divider">
               <span>or</span>
             </div>
-            <EmailLogin />
+            {/* <EmailLogin /> */}
           </div>
         </div>
       )}
